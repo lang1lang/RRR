28475b342d6dd175a6d6b26fb07205ea18b2bef3
lyfdamon <1715362583@qq.com>
2018-01-24 11:01:25 +0800

    The test script for SA algorithm

--- sa_test.sh
+++ sa_test.sh
@@ -7,5 +7,150 @@ code_types=("Cauchy_Orig" "Cauchy_Good" "Liberation" "Blaum_Roth" "Liber8tion")
 
 for i in {1..100}
 do
-	$(pwd)/bin/sa_test cauchy_orig 5 4 10 0
+	$(pwd)/bin/climb_test liber8tion 2 2 8 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test liber8tion 3 2 8 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test liber8tion 4 2 8 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test liber8tion 5 2 8 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test liber8tion 6 2 8 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test liber8tion 7 2 8 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test blaum_roth 2 2 6 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test blaum_roth 2 2 10 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test blaum_roth 2 2 12 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test blaum_roth 2 2 16 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test cauchy_good 2 2 10 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test cauchy_good 3 2 10 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test cauchy_good 4 2 10 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test cauchy_good 5 2 10 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test cauchy_good 6 2 10 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test cauchy_good 7 2 10 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test cauchy_good 8 2 10 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test cauchy_good 9 2 10 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test cauchy_good 5 4 10 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test cauchy_good 6 4 10 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test cauchy_good 7 4 10 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test cauchy_good 8 4 10 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test cauchy_good 8 3 10 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test cauchy_good 8 4 10 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test cauchy_good 8 5 10 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test cauchy_good 8 6 10 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test cauchy_good 8 4 10 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test cauchy_good 8 4 11 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test cauchy_good 8 4 12 0
+done
+
+for i in {1..100}
+do
+	$(pwd)/bin/climb_test cauchy_good 8 4 13 0
 done
