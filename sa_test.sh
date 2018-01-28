3981d5a8aeefdd3d4d4c8f51a12da145efd1fe59
lyfdamon <1715362583@qq.com>
2018-01-28 15:12:21 +0800

    The test script for SA algorithm

--- sa_test.sh
+++ sa_test.sh
@@ -3,8 +3,6 @@
 
 echo In this experiment, we will test the random replacement recovery algorithm:
 
-code_types=("Cauchy_Orig" "Cauchy_Good" "Liberation" "Blaum_Roth" "Liber8tion")
-
 for i in {1..300}
 do
 	$(pwd)/bin/sa_test cauchy_good 5 4 5 0	
