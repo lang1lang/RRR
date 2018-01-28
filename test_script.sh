defbed1d123933c72079f5f37ec0b3f23977c336
lyfdamon <1715362583@qq.com>
2018-01-28 15:13:49 +0800

    The test script for this project

---
+++ test_script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This is a test script for the simulation experiment of different algorithms and different erasure codes.
+bash climb_test.sh
+bash sa_test.sh
+bash enumberation_test.sh
