3c464bd4f10e95d588c3bc9c5cf2ae2153d4d1a7
lyfdamon <1715362583@qq.com>
2018-02-01 11:26:31 +0800

    The test script for SA algorithm

--- sa_test.sh
+++ sa_test.sh
@@ -5,20 +5,163 @@ echo In this experiment, we will test the random replacement recovery algorithm:
 
 for i in {1..300}
 do
-	$(pwd)/bin/sa_test cauchy_good 5 4 5 0	
+	$(pwd)/bin/sa_test cauchy_good 3 3 10 0	
 done
 
 for i in {1..300}
 do
-	$(pwd)/bin/sa_test cauchy_good 5 4 6 0
-done
-
-for i in {1..300}
-do	
-	$(pwd)/bin/sa_test cauchy_good 5 4 7 0
+	$(pwd)/bin/sa_test cauchy_good 4 3 10 0	
 done
 
 for i in {1..300}
 do
-	$(pwd)/bin/sa_test cauchy_good 5 4 8 0
+	$(pwd)/bin/sa_test cauchy_good 5 3 10 0	
 done
+
+for i in {1..300}
+do
+	$(pwd)/bin/sa_test cauchy_good 6 3 10 0	
+done
+
+for i in {1..300}
+do
+	$(pwd)/bin/sa_test cauchy_good 4 4 10 0	
+done
+
+for i in {1..300}
+do
+	$(pwd)/bin/sa_test cauchy_good 5 4 10 0	
+done
+
+for i in {1..300}
+do
+	$(pwd)/bin/sa_test cauchy_good 6 4 10 0	
+done
+
+for i in {1..300}
+do
+	$(pwd)/bin/sa_test cauchy_good 7 4 10 0	
+done
+
+for i in {1..300}
+do
+	$(pwd)/bin/sa_test cauchy_good 8 4 10 0	
+done
+
+for i in {1..300}
+do
+	$(pwd)/bin/sa_test cauchy_good 9 4 10 0	
+done
+
+for i in {1..300}
+do
+	$(pwd)/bin/sa_test cauchy_good 5 2 10 0	
+done
+
+for i in {1..300}
+do
+	$(pwd)/bin/sa_test cauchy_good 5 3 10 0	
+done
+
+for i in {1..300}
+do
+	$(pwd)/bin/sa_test cauchy_good 5 4 10 0	
+done
+
+for i in {1..300}
+do
+	$(pwd)/bin/sa_test cauchy_good 5 5 10 0	
+done
+
+for i in {1..300}
+do
+	$(pwd)/bin/sa_test cauchy_good 4 3 7 0	
+done
+
+for i in {1..300}
+do
+	$(pwd)/bin/sa_test cauchy_good 4 3 8 0	
+done
+
+for i in {1..300}
+do
+	$(pwd)/bin/sa_test cauchy_good 4 3 9 0	
+done
+
+for i in {1..300}
+do
+	$(pwd)/bin/sa_test cauchy_good 4 3 10 0	
+done
+
+for i in {1..300}
+do
+	$(pwd)/bin/sa_test cauchy_good 4 4 7 0	
+done
+
+for i in {1..300}
+do
+	$(pwd)/bin/sa_test cauchy_good 4 4 8 0	
+done
+
+for i in {1..300}
+do
+	$(pwd)/bin/sa_test cauchy_good 4 4 9 0	
+done
+
+for i in {1..300}
+do
+	$(pwd)/bin/sa_test cauchy_good 4 4 10 0	
+done
+
+for i in {1..300}
+do
+	$(pwd)/bin/sa_test cauchy_good 5 3 7 0	
+done
+
+for i in {1..300}
+do
+	$(pwd)/bin/sa_test cauchy_good 5 3 8 0	
+done
+
+for i in {1..300}
+do
+	$(pwd)/bin/sa_test cauchy_good 5 3 9 0	
+done
+
+for i in {1..300}
+do
+	$(pwd)/bin/sa_test cauchy_good 5 3 10 0	
+done
+
+for i in {1..300}
+do
+	$(pwd)/bin/sa_test cauchy_good 5 4 7 0	
+done
+
+for i in {1..300}
+do
+	$(pwd)/bin/sa_test cauchy_good 5 4 8 0	
+done
+
+for i in {1..300}
+do
+	$(pwd)/bin/sa_test cauchy_good 5 4 9 0	
+done
+
+for i in {1..300}
+do
+	$(pwd)/bin/sa_test cauchy_good 5 4 10 0	
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
